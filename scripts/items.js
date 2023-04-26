f0b42cdde486b4856e9cbcd218e4403433ccd8a3
Kitrr1x <127106904+Kitrr1x@users.noreply.github.com>
2023-04-26 11:31:47 +0800

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -1,17 +1,9 @@
-const goldOre = extendContent(Item, "goldOre", {});
-const goldIngot = extendContent(Item, "goldIngot", {});
-const shinyAlloy = extendContent(Item, "shinyAlloy", {});
-const palladium = extendContent(Item, "palladium", {});
-const apShell2 = extendContent(Item, "apShell2", {});
-const heShell2 = extendContent(Item, "heShell2", {});
-const rocket2 = extendContent(Item, "rocket2", {});
+const Iron = extendContent(Item, "Iron", {});
+const Cobalt = extendContent(Item, "Cobalt", {});
+const Chromium = extendContent(Item, "Chromium", {});
 
 module.exports = {
-  goldOre: goldOre,
-  goldIngot: goldIngot,
-  shinyAlloy: shinyAlloy,
-  palladium: palladium,
-  apShell2: apShell2,
-  heShell2: heShell2,
-  rocket2: rocket2
+  Iron: Iron,
+  Cobalt: Cobalt,
+  Chromium: Chromium,
 }
