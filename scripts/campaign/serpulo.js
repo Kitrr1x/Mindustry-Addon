1d6dc24e1100d0899c1964c0dae05bc34effdea5
Kitrr1x <127106904+Kitrr1x@users.noreply.github.com>
2023-04-26 11:35:38 +0800

    Update serpulo.js

--- scripts/campaign/serpulo.js
+++ scripts/campaign/serpulo.js
@@ -1,10 +1,10 @@
 //serpulo presets
-const goldMountains = new SectorPreset("goldMountains", Planets.serpulo, 172);
-goldMountains.captureWave = 30;
-goldMountains.difficulty = 4;
+const IronMountains = new SectorPreset("IronMountains", Planets.serpulo, 172);
+IronMountains.captureWave = 30;
+IronMountains.difficulty = 4;
 
-const goldMines = new SectorPreset("goldMines", Planets.serpulo, 205);
-goldMines.difficulty = 7;
+const CobaltMines = new SectorPreset("CobaltMines", Planets.serpulo, 205);
+CobaltMines.difficulty = 7;
 
 const abandonedDesert = new SectorPreset("abandonedDesert", Planets.serpulo, 157);
 abandonedDesert.captureWave = 30;
@@ -15,8 +15,8 @@ reignLair.captureWave = 30;
 reignLair.difficulty = 10;
 
 module.exports = {
-  goldMountains: goldMountains,
-  goldMines: goldMines,
+  IronMountains: IronMountains,
+  CobaltMines: CobaltMines,
   abandonedDesert: abandonedDesert,
   reignLair: reignLair
 };
